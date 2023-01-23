f59c2a2b249860e013e39aa8a2043ca514eb362c
Ire Alarape <ialarape@andrew.cmu.edu>
2023-01-23 09:49:13 -0500

    Fix fibRoute

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,12 +1,12 @@
 // Endpoint for querying the fibonacci numbers
-
+// import fibonacci from './fib';
 const fibonacci = require("./fib");
 
-export default (req, res) => {
-  const { num } = req.params;
+export default (req: { params: string; }, res: { send: (arg0: string) => void; }) => {
+  const num:string = req.params;
 
   const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  let result:string = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
